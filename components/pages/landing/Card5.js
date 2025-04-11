e54a4daac6248cd0065398c8583aee0a0cde7793
Viveks399 <vivek.me.619@gmail.com>
2025-04-11 10:53:48 +0530

    Added two components

---
+++ components/pages/landing/Card5.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination, Autoplay } from "swiper/modules";
+
+const Card5 = ({ item }) => {
+  return (
+    <div className="bg-white py-16 px-4 md:px-10">
+      <h2 className="text-3xl md:text-5xl font-extrabold mb-12 ml-[57px] text-black">
+        TAILORED FOR YOUR <span className="text-[#FF5900]">SECTOR</span>
+      </h2>
+
+      <Swiper
+        spaceBetween={20}
+        pagination={{ clickable: true }}
+        autoplay={{ delay: 2000, disableOnInteraction: false }}
+        loop={true}
+        modules={[Pagination, Autoplay]}
+        slidesPerView={1}
+        breakpoints={{
+          640: { slidesPerView: 2 },
+          1024: { slidesPerView: 4 },
+        }}
+        className="max-w-7xl mx-auto !pb-10"
+      >
+        {item?.map((sector, index) => (
+          <SwiperSlide key={index}>
+            <div className="flex flex-col items-center text-center bg-white p-4 ">
+              <img
+                src={sector.img}
+                alt={sector.title}
+                className="rounded-2xl w-full h-[400px] object-cover mb-4"
+              />
+              <h3 className="text-xl font-semibold mb-2 text-black">
+                {sector.title}
+              </h3>
+              <p className="text-gray-600 text-sm">{sector.para}</p>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default Card5;
